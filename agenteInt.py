fc9a873ca9a3906993cb82a387a226133a494edb
jhin4kill <zeus10090711@gmail.com>
2023-09-29 12:34:31 -0600

    222

--- agenteInt.py
+++ agenteInt.py
@@ -153,14 +153,6 @@ class Agentes:
                         new_x, new_y = n_x, n_y+1
                         continue
                     """
-<<<<<<< HEAD
-<<<<<<< HEAD
-                    
-=======
->>>>>>> b072ad8 (Jese cambios)
-=======
-                    
->>>>>>> cd47ff1 (pruebanagel)
 
                     if tablero[n_x][n_y] == 'T':
                         new_x, new_y = n_x, n_y 
